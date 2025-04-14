002a988a3ba3e35f279290e2e88486ea3f63afd8
ZhukovaPolina <aleksandr.rds@gmail.com>
2025-04-14 17:18:57 +0400

    Update Tetrahedron.cpp

--- Tetrahedron.cpp
+++ Tetrahedron.cpp
@@ -1,4 +1,4 @@
-﻿#include "Tetrahedron.h"
+#include "Tetrahedron.h"
 #include <stdexcept>
 #include <cmath>
 
@@ -9,18 +9,15 @@ Tetrahedron::Tetrahedron(const Point& point1, const Point& point2,
     if (point1 == point2 || point1 == point3 || point1 == point4 ||
         point2 == point3 || point2 == point4 || point3 == point4)
     {
-        throw std::logic_error("����� ���������, ��� �� ��������.");
+        throw std::logic_error("ошибка.");
     }
-
-    // ��������� ����� ������ ������������ (���������)
     side1 = point1.distanceTo(point2);
     side2 = point2.distanceTo(point3);
     side3 = point3.distanceTo(point1);
 
-    // ���������, ��� ����������� ����������
     if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
     {
-        throw std::logic_error("����� �� �������� �����������.");
+        throw std::logic_error("ошибка.");
     }
 }
 
@@ -41,7 +38,6 @@ double Tetrahedron::getSide3() const
 
 double Tetrahedron::baseArea() const
 {
-    // ������� ������ ��� ������� ������������
     double halfPerimeter = (side1 + side2 + side3) / 2;
     return std::sqrt(halfPerimeter * (halfPerimeter - side1) *
         (halfPerimeter - side2) * (halfPerimeter - side3));
